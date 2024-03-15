19999ebc72ffda657dbe95093939a8a87597b6ac
Yohan0412 <Yohan.rosano@hotmail.fr>
2024-03-15 17:00:07 +0100

    firt commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,53 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): object {
     return this.appService.getHello();
   }
+
+  @Get('/skatersWASHINGTON')
+  getSkatersWashington(): object {
+    
+    return this.appService.getSkatersWashington();
+  }
+
+  @Get('/skatersTORONTO')
+  getSkatersToronto(): object {
+    
+    return this.appService.getSkatersToronto();
+  }
+
+
+
+  @Get('/BestPlayer')
+  getBestPlayer(): object {
+    return this.appService.getBestPlayer();
+  }
+
+
+
+
+  private apiUrl = 'https://api-web.nhle.com/v1/club-stats/WSH/now';
+
+  async getSkaters(): Promise<object> {
+    const rep = await fetch(this.apiUrl, { method: 'GET' });
+    const response = await rep.json();
+    return response.skaters;
+  }
+
+
+
+  @Get('/skaters/search-by-name')
+  getFirstSkater(@Query('name') name): object {
+    return this.appService.getFirstSkater(name);
+  }
+
+  
 }
+
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,55 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  private apiUrl = 'https://api-web.nhle.com/v1/club-stats/WSH/now';
+  private apiUrl1 = 'https://api-web.nhle.com/v1/club-stats/TOR/now';
+  private apiUrlBestPlayer = 'https://api-web.nhle.com/v1/skater-stats-leaders/current?categories=goals&limit=5';
+
+
+
+  getHello(): object {
+    return {'api': '1.0.0'};
   }
+
+  async getSkatersWashington(): Promise<object> {
+    const rep = await fetch(this.apiUrl, { method: 'GET' });
+    const response = await rep.json();
+    return response.skaters;
+  }
+
+
+  async getBestPlayer(): Promise<object> {
+    const rep = await fetch(this.apiUrlBestPlayer, { method: 'GET' });
+    const response = await rep.json();
+    return response;
+  }
+
+  async getSkatersToronto(): Promise<object> {
+    const rep = await fetch(this.apiUrl1 , { method: 'GET' });
+    const response = await rep.json();
+    return response.skaters;
+  }
+
+
+
+
+
+
+
+
+  ///http://localhost:4000/skaters/search-by-name?name=Miroshnichenko
+  async getFirstSkater(name:string): Promise<object> {
+    const rep = await fetch(this.apiUrl, { method: 'GET' });
+    const response = await rep.json();
+    const skaters = response.skaters;
+    const player =  skaters.find(players => players.lastName.default === name);
+    return player;
+  }
+
+
+
+
+
 }
+
