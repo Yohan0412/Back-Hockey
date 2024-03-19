6da521652675913388b898763ab9313d4a6ea573
trini-tA <frederic.gacon@free.fr>
2024-03-19 22:18:50 +0100

    Code review

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,53 +1,30 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
-
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): object {
-    return this.appService.getHello();
+  getApi(): object {
+    return this.appService.getApi();
   }
 
-  @Get('/skatersWASHINGTON')
-  getSkatersWashington(): object {
-    
-    return this.appService.getSkatersWashington();
+  //http://localhost:4000/skaters/city/TORONTO
+  @Get('/skaters/city/:city')
+  getSkaters(@Param() params: any): object {
+    return this.appService.getSkatersByCity(params.city);
   }
 
-  @Get('/skatersTORONTO')
-  getSkatersToronto(): object {
-    
-    return this.appService.getSkatersToronto();
+  // http://localhost:4000/skaters/city/TORONTO/name/Giordano
+  @Get('/skaters/city/:city/name/:name')
+  getFirstSkater(@Param() params: any): object {
+    return this.appService.getSkaterByName(params.city, params.name);
   }
 
-
-
+  // http://localhost:4000/BestPlayer
   @Get('/BestPlayer')
   getBestPlayer(): object {
     return this.appService.getBestPlayer();
   }
-
-
-
-
-  private apiUrl = 'https://api-web.nhle.com/v1/club-stats/WSH/now';
-
-  async getSkaters(): Promise<object> {
-    const rep = await fetch(this.apiUrl, { method: 'GET' });
-    const response = await rep.json();
-    return response.skaters;
-  }
-
-
-
-  @Get('/skaters/search-by-name')
-  getFirstSkater(@Query('name') name): object {
-    return this.appService.getFirstSkater(name);
-  }
-
-  
 }
-
